2fcc9ceff260c901c29d654a281cbc988ab87742
Harvey Tang <harvey.xw.tang@gmail.com>
2016-02-24 12:59:09 -0500

    Wrote data gatherer and neural net analysis (need to test) to see if different finger arrangements can be detected. No filtering / preprocessing currently being done on EMG data before being fed to neural net - should we change this?

--- gesture_classifier5.py
+++ gesture_classifier5.py
@@ -1,27 +1,42 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
+import pickle
 
-model = Sequential()
-# Dense(64) is a fully-connected layer with 64 hidden units.
-# in the first layer, you must specify the expected input data shape:
-# here, 20-dimensional vectors.
-model.add(Dense(64, input_dim=8, init='uniform'))
-model.add(Activation('tanh'))
-model.add(Dropout(0.5))
-model.add(Dense(64, init='uniform'))
-model.add(Activation('tanh'))
-model.add(Dropout(0.5))
-model.add(Dense(15, init='uniform'))
-model.add(Activation('softmax'))
 
-sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy',
-              optimizer=sgd)
+class nur_net(object):
+    model = None
 
-# Need X and Y data here
-model.fit(X_train, y_train,
-          nb_epoch=20,
-          batch_size=16,
-          show_accuracy=True)
-score = model.evaluate(X_test, y_test, batch_size=16)
+    def __init__(self):
+        model = Sequential()
+        # Dense(64) is a fully-connected layer with 64 hidden units.
+        # in the first layer, you must specify the expected input data shape:
+        # here, 20-dimensional vectors.
+        model.add(Dense(32, input_dim=8, init='uniform'))
+        model.add(Activation('tanh'))
+        model.add(Dropout(0.5))
+        model.add(Dense(32, init='uniform'))
+        model.add(Activation('tanh'))
+        model.add(Dropout(0.5))
+        model.add(Dense(5, init='uniform'))
+        model.add(Activation('softmax'))
+
+        sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=sgd)
+        self.model = model
+
+    def train(self, x, y):
+        self.model.fit(x, y, nb_epoch=30, batch_size=1000, show_accuracy=True)
+
+    def accuracy(self, x, y):
+        return self.model.evaluate(x, y, batch_size=1000)
+
+
+def main():
+    samples = pickle.load(open('myo_data.pkl', 'r'))
+    myo_net = nur_net()
+    myo_net.train(samples['data'], samples['labels'])
+
+if __name__ == "__main__":
+    main()
