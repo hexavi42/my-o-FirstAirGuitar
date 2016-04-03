3fa727bb4e616293ba9386a850e57fc0fbcf4652
Ryan Ong <ryan.ong@sympatico.ca>
2016-04-02 22:40:39 -0400

    Improve authenticity of chord sounds

--- play_chords.py
+++ play_chords.py
@@ -22,17 +22,19 @@ class chord:
     def __init__(self, *args):
         self.notes = args
     def play(self, duration=3):
+        delay = 0
         for i in self.notes:
-            subprocess.Popen(['play', '-n', 'synth', str(duration) , 'pluck', i], stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
+            subprocess.Popen(['play', '-n', 'synth', 'pluck', i, 'delay', str(delay),'fade', '0', str(duration), '.1', 'norm', '-1'], stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
+            delay = delay + 0.01
 
 # We can also define sharps and flats:
 # G# or Gb
 # All notes are relative to middle A (440 hz)
 
-g_chord = chord('G3', 'B3', 'D')
-d_chord = chord('D', 'F#', 'A')
-em_chord = chord('E','G','B')
-c_chord = chord('C','E','G')
+g_chord = chord('G3', 'B3', 'D3', 'G3', 'B3', 'G')
+d_chord = chord('D3', 'A3', 'D4', 'F#4')
+em_chord = chord('E2', 'B2', 'E3', 'G3', 'B3', 'E4')
+c_chord = chord('C3', 'E3', 'G3', 'C4', 'E4')
 
 chord_dict = {'C': g_chord, 'D': d_chord, 'E': em_chord, 'G': c_chord}
 
